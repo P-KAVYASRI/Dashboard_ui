56451bcc955f8277387f0ba6e1acfe159ba57df4
P-KAVYASRI <kavyasri369369@gmail.com>
2025-09-07 00:20:06 +0530

    commit

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,46 @@
+
+// Update the import path if the file is named differently or located elsewhere
+import AttendanceChart from "@/components/AttendenceChart";
+// Example: If the file is named AttendanceChart.tsx and located in src/components, use:
+// import AttendanceChart from "../../components/AttendanceChart";
+import CountChart from "@/components/CountChart";
+import EventCalendar from "@/components/EventCalender";
+import FinanceChart from "@/components/FinanceChart";
+import UserCard from "@/components/UserCard";
+
 const AdminPage = () => {
   return (
-    <div>
-      <h1>Admin Dashboard</h1>
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+        {/* MIDDLE CHARTS */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* COUNT CHART */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart />
+          </div>
+        </div>
+        {/* BOTTOM CHART */}
+        <div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+       
+      </div>
     </div>
   );
 };
