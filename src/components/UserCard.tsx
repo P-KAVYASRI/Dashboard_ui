d908ef2ad0fb817719191b00ef8436d541341b30
P-KAVYASRI <kavyasri369369@gmail.com>
2025-09-06 10:43:30 +0530

    commit

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,11 @@
+const UserCard = ({ user }) => {
+  return (
+    <div className="user-card">
+      <img src={user.avatar} alt={user.name} />
+      <h2>{user.name}</h2>
+      <p>{user.email}</p>
+    </div>
+  );
+};
+
+export default UserCard;
