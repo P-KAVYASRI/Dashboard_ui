e2d1e5e97f36441dbfe931c232a3e9c072d811d5
P-KAVYASRI <kavyasri369369@gmail.com>
2025-09-08 23:33:31 +0530

    msg

---
+++ src/components/BigCalender.tsx
@@ -0,0 +1,11 @@
+const BigCalendar = () => {
+  return (
+    <div>
+      <h2 className="text-lg font-semibold">Big Calendar</h2>
+      {/* Calendar implementation goes here */}
+    </div>
+  );
+};
+
+export default BigCalendar;
+
